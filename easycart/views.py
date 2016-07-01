6c53d5d7e64f4f3563de53c91be73e9a8ec52462
nevimov <nevimov@gmail.com>
2016-07-01 21:51:45 +0700

    easycart.views: Rewrite module
    * Add a base class for all views
    * Move common functionality to this base class
    * Add silent exception handling (error is sent in JsonResponse)
    * Update module docstring
    * Amend docstrings of existing views

--- easycart/views.py
+++ easycart/views.py
@@ -5,6 +5,20 @@ representation. For the details on the format of the return value,
 see the :meth:`~easycart.cart.BaseCart.encode` method of the
 :class:`~easycart.cart.BaseCart` class.
 
+If a parameter required by a view is not present in the request's POST
+data, then the JSON-response will have the format::
+
+   {'error': 'MissingRequestParam', 'param': parameter_name}
+
+Almost the same thing happens, if a parameter is invalid and results in
+an exception, which is a subclass of :class:`~easycart.cart.CartException`.
+In this case, the error value will be the name of the concrete exception
+class (e.g. ``'ItemNotInCart'`` or ``'NegativeItemQuantity'``).
+And instead of ``param`` there may be one or more items providing
+additional info on the error, for example, the primary key of an item
+you was trying to change or an invalid quantity passed in the request.
+
+
 Note
 ----
 All of the views in this module accept only POST requests.
@@ -13,7 +27,10 @@ All of the views in this module accept only POST requests.
 from importlib import import_module
 
 from django.conf import settings
+from django.http import JsonResponse
 from django.views.generic import View
+
+from easycart.cart import CartException
 
 __all__ = [
     'AddItem',
@@ -26,7 +43,46 @@ cart_module, cart_class = settings.EASYCART_CART_CLASS.rsplit('.', 1)
 Cart = getattr(import_module(cart_module), cart_class)
 
 
-class AddItem(View):
+class CartView(View):
+    """Base class for views operating the cart."""
+    action = None
+    """Attribute of the cart object, which will be called to perform
+    some action on the cart.
+    """
+    required_params = ()
+    """Iterable of parameters, which MUST be present in the post data."""
+    optional_params = {}
+    """Dictionary of parameters, which MAY be present in the post data.
+
+    Parameters serve as keys. Associated values will be used as fallbacks
+    in case the parameter is not in the post data.
+    """
+
+    def post(self, request):
+        # Extract parameters from the post data
+        params = {}
+        for param in self.required_params:
+            try:
+                params[param] = request.POST[param]
+            except KeyError:
+                return JsonResponse({
+                    'error': 'MissingRequestParam',
+                    'param': param,
+                })
+        for param, fallback in self.optional_params.items():
+            params[param] = request.POST.get(param, fallback)
+        # Perform an action on the cart using these parameters
+        cart = Cart(request)
+        action = getattr(cart, self.action)
+        try:
+            action(**params)
+        except CartException as exc:
+            return JsonResponse(dict({'error': exc.__class__.__name__},
+                                     **exc.kwargs))
+        return cart.encode()
+
+
+class AddItem(CartView):
     """Add an item to the cart.
 
     This view expects `request.POST` to contain:
@@ -35,22 +91,20 @@ class AddItem(View):
     | key        | value                                              |
     +============+====================================================+
     | `pk`       | the primary key of an item to add                  |
-    +------+-----+----------------------------------------------------+
+    +------------+----------------------------------------------------+
     | `quantity` | a quantity that should be associated with the item |
     +------------+----------------------------------------------------+
 
+    The `quantity` parameter is optional (defaults to 1).
+
     """
-
-    def post(self, request):
-        cart = Cart(request)
-        pk = request.POST['pk']
-        quantity = request.POST['quantity']
-        cart.add(pk, quantity)
-        return cart.encode()
+    action = 'add'
+    required_params = ('pk',)
+    optional_params = {'quantity': 1}
 
 
-class ChangeItemQuantity(View):
-    """Change the quantity of an item.
+class ChangeItemQuantity(CartView):
+    """Change the quantity associated with an item.
 
     This view expects `request.POST` to contain:
 
@@ -58,39 +112,26 @@ class ChangeItemQuantity(View):
     | key        | value                                              |
     +============+====================================================+
     | `pk`       | the primary key of an item                         |
-    +------+-----+----------------------------------------------------+
+    +------------+----------------------------------------------------+
     | `quantity` | a new quantity to associate with the item          |
     +------------+----------------------------------------------------+
 
     """
-
-    def post(self, request):
-        cart = Cart(request)
-        pk = request.POST['pk']
-        quantity = request.POST['quantity']
-        cart.change_quantity(pk, quantity)
-        return cart.encode()
+    action = 'change_quantity'
+    required_params = ('pk', 'quantity')
 
 
-class RemoveItem(View):
+class RemoveItem(CartView):
     """Remove an item from the cart.
 
     Expects `request.POST` to contain key *pk*. The associated value
     should be the primary key of an item you wish to remove.
 
     """
-
-    def post(self, request):
-        cart = Cart(request)
-        pk = request.POST['pk']
-        cart.remove(pk)
-        return cart.encode()
+    action = 'remove'
+    required_params = ('pk',)
 
 
-class EmptyCart(View):
+class EmptyCart(CartView):
     """Remove all items from the cart."""
-
-    def post(self, request):
-        cart = Cart(request)
-        cart.empty()
-        return cart.encode()
+    action = 'empty'
