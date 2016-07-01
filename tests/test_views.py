a3c8e38e19cd6af304f50d6b93b4ba751b9c3b04
nevimov <nevimov@gmail.com>
2016-07-02 00:20:44 +0700

    tests/test_views: Test handling of invalid POST data

--- tests/test_views.py
+++ tests/test_views.py
@@ -33,6 +33,66 @@ class TestViews(TestCase):
              'totalPrice': '116.00'}
         )
 
+    def test_add_item_without_quantity(self):
+        """If post data don't contain parameter 'quantity', then one unit of a
+        requested item should be added (the quantity defaults to 1).
+        """
+        self.check_response(
+            'cart-add',
+            {'pk': 1},
+            {'items': {'1': {'price': '3.00', 'quantity': 11, 'total': '33.00'},
+                       '2': {'price': '5.00', 'quantity': 12, 'total': '60.00'},
+                       '3': {'price': '1.50', 'quantity': 6, 'total': '9.00'},
+                       '4': {'price': '2.00', 'quantity': 1, 'total': '2.00'}},
+             'itemCount': 4,
+             'totalPrice': '104.00'}
+        )
+
+    def test_add_item_without_pk(self):
+        """If post data don't contain parameter 'pk', no exception should be
+        raised and the response is expected to contain the relevant error
+        message.
+        """
+        self.check_response(
+            'cart-add',
+            {},
+            {'error': 'MissingRequestParam', 'param': 'pk'}
+        )
+
+    def test_add_item_with_negative_quantity(self):
+        """If post data contain parameter 'quantity' and the associated value
+        is negative, then the corresponding exception is expected to be handled
+        by the view, and the response should contain the relevant error message.
+        """
+        self.check_response(
+            'cart-add',
+            {'pk': 1, 'quantity': -1},
+            {'error': 'NegativeItemQuantity', 'quantity': -1}
+        )
+
+    def test_add_item_with_zero_quantity(self):
+        """If post data contain parameter 'quantity' and the associated value
+        is zero, then the corresponding exception is expected to be handled by
+        the view, and the response should contain the relevant error message.
+        """
+        self.check_response(
+            'cart-add',
+            {'pk': 1, 'quantity': 0},
+            {'error': 'ZeroItemQuantity'}
+        )
+
+    def test_add_item_with_non_convertible_quantity(self):
+        """If post data contain parameter 'quantity' and the associated value
+        can't be converted to an integer, then the corresponding exception is
+        expected to be handled by the view, and the response should contain the
+        relevant error message.
+        """
+        self.check_response(
+            'cart-add',
+            {'pk': 1, 'quantity': 'xxx'},
+            {'error': 'NonConvertibleItemQuantity', 'quantity': 'xxx'}
+        )
+
     def test_remove_item(self):
         self.check_response(
             'cart-remove',
@@ -42,6 +102,17 @@ class TestViews(TestCase):
                        '4': {'price': '2.00', 'quantity': 1, 'total': '2.00'}},
              'itemCount': 3,
              'totalPrice': '71.00'}
+        )
+
+    def test_remove_item_without_pk(self):
+        """If post data don't contain parameter 'pk', then the corresponding
+        exception is expected to be handled by the view, and the response
+        should contain the relevant error message.
+        """
+        self.check_response(
+            'cart-remove',
+            {},
+            {'error': 'MissingRequestParam', 'param': 'pk'}
         )
 
     def test_change_item_quantity(self):
@@ -55,6 +126,52 @@ class TestViews(TestCase):
              'itemCount': 4,
              'totalPrice': '86.00'}
         )
+
+    def test_change_item_without_pk(self):
+        """If post data don't contain parameter 'pk', no exception should be
+        raised and the response is expected to contain the relevant error
+        message.
+        """
+        self.check_response(
+            'cart-change-quantity',
+            {},
+            {'error': 'MissingRequestParam', 'param': 'pk'}
+        )
+
+    def test_change_item_with_negative_quantity(self):
+        """If post data contain parameter 'quantity' and the associated value
+        is negative, then the corresponding exception is expected to be handled
+        by the view, and the response should contain the relevant error message.
+        """
+        self.check_response(
+            'cart-change-quantity',
+            {'pk': 1, 'quantity': -1},
+            {'error': 'NegativeItemQuantity', 'quantity': -1}
+        )
+
+    def test_change_item_with_zero_quantity(self):
+        """If post data contain parameter 'quantity' and the associated value
+        is zero, then the corresponding exception is expected to be handled by
+        the view, and the response should contain the relevant error message.
+        """
+        self.check_response(
+            'cart-change-quantity',
+            {'pk': 1, 'quantity': 0},
+            {'error': 'ZeroItemQuantity'}
+        )
+
+    def test_change_item_with_non_convertible_quantity(self):
+        """If post data contain parameter 'quantity' and the associated value
+        can't be converted to an integer, then the corresponding exception is
+        expected to be handled by the view, and the response should contain the
+        relevant error message.
+        """
+        self.check_response(
+            'cart-change-quantity',
+            {'pk': 1, 'quantity': 'xxx'},
+            {'error': 'NonConvertibleItemQuantity', 'quantity': 'xxx'}
+        )
+
 
     def test_empty_cart(self):
         self.check_response(
